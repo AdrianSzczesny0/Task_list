fbf87f4c9c6ffb3c828d2ee8918b6bcf8760a031
aszczesny <aszczesny@beeline.com>
2024-04-17 20:57:08 +0200

    Added notification bar with success and fail

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ const item_container = document.getElementById("list");
 const item_text = document.getElementById("itemName");
 const removeBtn = document.getElementsByClassName;
 const itemList = [];
+const notybar = document.getElementById("noty_bar");
 
 console.log(addBtn);
 
+
+// add new list item 
 function addNewListItem(){
   const toAddName = item_text.value;
   if (itemList.includes(toAddName) || toAddName==""){
     console.log('item already in list');
     document.getElementById("itemName").value = "";
+    createNotybar('fail','Item already exists');
   }else{
     const toAddName = item_text.value;
     const clonedItem = item_template.content.cloneNode(true);
@@ -28,6 +32,9 @@ function addNewListItem(){
         }
       }
     }
+    createNotybar('success','Successfully added item');
+
+    // item check uncheck
     clonedItemName.onclick = function() {
       console.log('CLICKED');
       if (clonedItemName.parentElement.classList.contains('Finished')){
@@ -70,6 +77,53 @@ document.body.addEventListener("keydown", (ev) =>{
   }
 });
 
+
+async function createNotybar(type,message){
+  console.log('Creating notybar');
+  const notyBar = document.createElement('div');
+  const notyBarMessage = document.createElement('p');
+  
+  notyBar.classList.add('noty_bar');
+  if (type=='success'){
+    notyBar.classList.add(type);
+    notyBarMessage.innerHTML = message;
+  }else if (type=="fail"){
+    notyBar.classList.add(type);
+    notyBarMessage.innerHTML = message;
+  }else{
+    console.log(`There is no notification type ${type}`)
+  }
+  notyBar.appendChild(notyBarMessage);
+  document.body.appendChild(notyBar);
+  await fadeIn(notyBar);
+  await delay(1000);
+  await fadeout(notyBar);
+  notyBar.remove();
+}
+
+async function fadeIn(element){
+  for (let index = 0; index <10; index++) {
+    await delay(20);
+    element.style.opacity = index/10;
+  }
+}
+async function fadeout(element){
+  for (let index = 10; index <0; index--) {
+    await delay(20);
+    element.style.opacity = index/10;
+  }
+}
+
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function test() {
+  console.log('start timer');
+  await delay(1000);
+  console.log('after 1 second');
+}
 
 // const addButton = document.getElementById('sub-btn');
 // const listContainer = document.getElementById('items');
