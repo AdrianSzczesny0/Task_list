a35f177f593f2d847ea74dbfec121e032433352b
aszczesny <aszczesny@beeline.com>
2024-04-18 11:45:17 +0200

    update to RWD

--- main.js
+++ main.js
@@ -17,11 +17,13 @@ function addNewListItem(){
     document.getElementById("itemName").value = "";
     createNotybar('fail','Item already exists');
   }else{
+    const firstItem = document.getElementsByClassName("")
     const toAddName = item_text.value;
     const clonedItem = item_template.content.cloneNode(true);
     const clonedItemName = clonedItem.getElementById("item_name");
     const delete_button = clonedItem.getElementById("item_button");
     clonedItemName.innerHTML = toAddName;
+    clonedItemName.parentElement.classList.add('visible');
     item_container.append(clonedItem);
     document.getElementById("itemName").value = "";
     delete_button.onclick = function() {
@@ -124,40 +126,3 @@ async function test() {
   await delay(1000);
   console.log('after 1 second');
 }
-
-// const addButton = document.getElementById('sub-btn');
-// const listContainer = document.getElementById('items');
-// const inputField = document.getElementById('sub');
-// const rmvButton = document.getElementById('rmv');
-
-// const handleRemove = e => {
-//   const item = e.target.closest('.item');
-  
-//   // Remove the listener, to avoid memory leaks.
-//   item.querySelector('.remove-btn')
-//     .removeEventListener('click', handleRemove);
-  
-//   item.parentElement.removeChild(item);
-// };
-
-// // Adds items to list after clicking button
-// addButton.addEventListener('click', e => {
-//   const item = document.createElement('div');
-//   const paragraph = document.createElement('div');
-//   const remove = document.createElement('button');
-  
-//   item.classList.add('item');
-//   paragraph.classList.add('paragraph-style');
-//   remove.classList.add('remove-btn');
-  
-//   paragraph.textContent = inputField.value;
-//   remove.textContent = 'Remove';
-  
-//   remove.addEventListener('click', handleRemove);
-  
-//   item.append(paragraph);
-//   item.append(remove);
-//   listContainer.append(item);
-  
-//   inputField.value = '';
-// })
